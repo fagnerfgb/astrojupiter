197a5108ca55f065264ba228f1b6d91f0280714f
Fagner Geraldes Braga <fagner.fgb@gmail.com>
2022-09-25 15:20:04 -0300

    Atualizando a documentação

---
+++ tarot.js
@@ -0,0 +1,78 @@
+/**
+ * Baralho de Tarot
+ * @Author: Fagner Geraldes Braga
+ */
+
+var sorteio = Math.floor(Math.random() * 22 + 1);
+
+switch (sorteio) {
+	case 1:
+		document.getElementById("carta").src = "img/tarot1.png";
+		break;
+	case 2:
+		document.getElementById("carta").src = "img/tarot2.png";
+		break;
+	case 3:
+		document.getElementById("carta").src = "img/tarot3.png";
+		break;
+	case 4:
+		document.getElementById("carta").src = "img/tarot4.png";
+		break;
+	case 5:
+		document.getElementById("carta").src = "img/tarot5.png";
+		break;
+	case 6:
+		document.getElementById("carta").src = "img/tarot6.png";
+		break;
+	case 7:
+		document.getElementById("carta").src = "img/tarot7.png";
+		break;
+	case 8:
+		document.getElementById("carta").src = "img/tarot8.png";
+		break;
+	case 9:
+		document.getElementById("carta").src = "img/tarot9.png";
+		break;
+	case 10:
+		document.getElementById("carta").src = "img/tarot10.png";
+		break;
+	case 11:
+		document.getElementById("carta").src = "img/tarot11.png";
+		break;
+	case 12:
+		document.getElementById("carta").src = "img/tarot12.png";
+		break;
+	case 13:
+		document.getElementById("carta").src = "img/tarot13.png";
+		break;
+	case 14:
+		document.getElementById("carta").src = "img/tarot14.png";
+		break;
+	case 15:
+		document.getElementById("carta").src = "img/tarot15.png";
+		break;
+	case 16:
+		document.getElementById("carta").src = "img/tarot16.png";
+		break;
+	case 17:
+		document.getElementById("carta").src = "img/tarot17.png";
+		break;
+	case 18:
+		document.getElementById("carta").src = "img/tarot18.png";
+		break;
+	case 19:
+		document.getElementById("carta").src = "img/tarot19.png";
+		break;
+	case 20:
+		document.getElementById("carta").src = "img/tarot20.png";
+		break;
+	case 21:
+		document.getElementById("carta").src = "img/tarot21.png";
+		break;
+	case 22:
+		document.getElementById("carta").src = "img/tarot22.png";
+		break;
+	default:
+	//caso contrário	
+	break;
+}
